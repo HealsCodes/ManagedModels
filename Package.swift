ee197a4b51a05c784fa05c7b9040b0eeb8c483fb
Renée Köcher <heals@codes.heals-draws.art>
2024-10-27 01:55:41 +0200

    Use swift-data-xcframeworks version 600.0.1

--- Package.swift
+++ Package.swift
@@ -1,49 +1,53 @@
-// swift-tools-version: 5.9
-import PackageDescription
+// swift-tools-version: 6.0
 import CompilerPluginSupport
+import PackageDescription
 
 #if swift(>=5.10)
-let settings = [ SwiftSetting.enableExperimentalFeature("StrictConcurrency") ]
+    let settings = [SwiftSetting.enableExperimentalFeature("StrictConcurrency")]
 #else
-let settings = [ SwiftSetting ]()
+    let settings = [SwiftSetting]()
 #endif
 
 let package = Package(
-  name: "ManagedModels",
-  
-  platforms: [ .macOS(.v11), .iOS(.v13), .tvOS(.v13), .watchOS(.v6) ],
-  products: [
-    .library(name: "ManagedModels", targets: [ "ManagedModels" ])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0")
-  ],
-  targets: [
-    .target(
-      name: "ManagedModels",
-      dependencies: [ "ManagedModelMacros" ],
-      swiftSettings: settings
-    ),
+    name: "ManagedModels",
 
-    .macro(
-      name: "ManagedModelMacros",
-      dependencies: [
-        .product(name: "SwiftSyntaxMacros",   package: "swift-syntax"),
-        .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-      ]
-    ),
-    
-    .testTarget(
-      name: "ManagedModelTests",
-      dependencies: [ "ManagedModels" ]
-    ),
-    
-    .testTarget(
-      name: "ManagedModelMacrosTests",
-      dependencies: [
-        "ManagedModelMacros",
-        .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
-      ]
-    ),
-  ]
+    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17), .watchOS(.v10)],
+    products: [
+        .library(name: "ManagedModels", targets: ["ManagedModels"])
+    ],
+    dependencies: [
+        // TODO: switch to the official repo once 600.0.1 is merged
+        //.package(url: "https://github.com/sjavora/swift-syntax-xcframeworks.git", from: "510.0.0")
+        .package(
+            url: "https://github.com/openium/swift-syntax-xcframeworks.git",
+            branch: "feat/swift-6-public-import-and-release")
+    ],
+    targets: [
+        .target(
+            name: "ManagedModels",
+            dependencies: ["ManagedModelMacros"],
+            swiftSettings: settings
+        ),
+
+        .macro(
+            name: "ManagedModelMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxWrapper", package: "swift-syntax-xcframeworks")
+            ]
+        ),
+
+        .testTarget(
+            name: "ManagedModelTests",
+            dependencies: ["ManagedModels"]
+        ),
+
+        .testTarget(
+            name: "ManagedModelMacrosTests",
+            dependencies: [
+                "ManagedModelMacros",
+                .product(name: "SwiftSyntaxWrapper", package: "swift-syntax-xcframeworks"),
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.version("6")]
 )
--- Sources/ManagedModelMacros/Models/ModelProperty.swift
+++ Sources/ManagedModelMacros/Models/ModelProperty.swift
@@ -6,60 +6,58 @@
 import SwiftSyntax
 import SwiftSyntaxMacros
 
-private let forbiddenPropertyNames : Set<String> = [
-  "context", "entity", "managedObjectContext", "objectID",
-  "description",
-  "inserted", "updated", "deleted", "hasChanges", "hasPersistentChangedValues",
-  "isFault"
+private let forbiddenPropertyNames: Set<String> = [
+    "context", "entity", "managedObjectContext", "objectID",
+    "description",
+    "inserted", "updated", "deleted", "hasChanges", "hasPersistentChangedValues",
+    "isFault",
 ]
 
-/**
- * Information about a variable that we detected as a property.
- */
+/// Information about a variable that we detected as a property.
 struct ModelProperty {
-  
-  /**
+
+    /**
    * Whether the property is an attribute or relationship,
    * or whether it is unknown at macro expansion time.
    */
-  enum PropertyType {
-    
-    /// The property had no `@Relationship` or `@Attribute` marker macros.
-    case plain
-    
-    /// The property was explicitly tagged as a `Relationship`.
-    case relationship(AttributeSyntax)
-    
-    /// The property was explicitly tagged as an `Attribute`.
-    case attribute   (AttributeSyntax, isTransformable: Bool)
-  }
-  
-  let binding           : PatternBindingSyntax
-  
-  // Extracted information
-  let type              : PropertyType
-  let name              : String
+    enum PropertyType {
 
-  let declaredValueType : TypeSyntax?
+        /// The property had no `@Relationship` or `@Attribute` marker macros.
+        case plain
 
-  let isTransient       : Bool
-  let initExpression    : ExprSyntax?
-}
+        /// The property was explicitly tagged as a `Relationship`.
+        case relationship(AttributeSyntax)
 
-extension ModelProperty {
-  
-  var isTransformable : Bool {
-    switch type {
-      case .plain           : return false
-      case .relationship(_) : return false
-      case .attribute(_, isTransformable: let value): return value
+        /// The property was explicitly tagged as an `Attribute`.
+        case attribute(AttributeSyntax, isTransformable: Bool)
     }
-  }
+
+    let binding: PatternBindingSyntax
+
+    // Extracted information
+    let type: PropertyType
+    let name: String
+
+    let declaredValueType: TypeSyntax?
+
+    let isTransient: Bool
+    let initExpression: ExprSyntax?
 }
 
 extension ModelProperty {
-  
-  /**
+
+    var isTransformable: Bool {
+        switch type {
+        case .plain: return false
+        case .relationship(_): return false
+        case .attribute(_, isTransformable: let value): return value
+        }
+    }
+}
+
+extension ModelProperty {
+
+    /**
    * If the property type was not declared, attempt to derive the type from
    * expression.
    *
@@ -69,79 +67,81 @@ extension ModelProperty {
    * ```
    * => type will be `String`, because it is a String literal initializer.
    */
-  var valueType: TypeSyntax? {
-    if let declaredValueType { return declaredValueType }
-    
-    guard let initExpression else { return nil }
-    return initExpression.detectExpressionType()
-  }
-  
-  var isKnownAttributePropertyType: Bool {
-    switch type {
-      case .attribute(_, _) : return true
-      case .relationship(_) : return false
-      case .plain: return valueType?.isKnownAttributePropertyType ?? false
+    var valueType: TypeSyntax? {
+        if let declaredValueType { return declaredValueType }
+
+        guard let initExpression else { return nil }
+        return initExpression.detectExpressionType()
     }
-  }
-  var isKnownRelationshipPropertyType: Bool {
-    switch type {
-      case .attribute(_, _) : return false
-      case .relationship(_) : return true
-      case .plain: return valueType?.isKnownRelationshipPropertyType ?? false
+
+    var isKnownAttributePropertyType: Bool {
+        switch type {
+        case .attribute(_, _): return true
+        case .relationship(_): return false
+        case .plain: return valueType?.isKnownAttributePropertyType ?? false
+        }
     }
-  }
+    var isKnownRelationshipPropertyType: Bool {
+        switch type {
+        case .attribute(_, _): return false
+        case .relationship(_): return true
+        case .plain: return valueType?.isKnownRelationshipPropertyType ?? false
+        }
+    }
 }
 
 extension ModelProperty: CustomStringConvertible {
-  
-  var description: String {
-    var ms = "<ModelProp[\(name)]:"
-    ms += " \(type)"
-    if isTransient { ms += " transient" }
-    
-    if let valueType      { ms += " type=\(valueType)" }
-    if let initExpression { ms += " init=\(initExpression)" }
-    ms += ">"
-    return ms
-  }
+
+    var description: String {
+        var ms = "<ModelProp[\(name)]:"
+        ms += " \(type)"
+        if isTransient { ms += " transient" }
+
+        if let valueType { ms += " type=\(valueType)" }
+        if let initExpression { ms += " init=\(initExpression)" }
+        ms += ">"
+        return ms
+    }
 }
 
 extension ModelProperty.PropertyType: CustomStringConvertible {
-  
-  var description: String {
-    switch self {
-      case .plain           : return "plain"
-      case .relationship(_) : return "Relationship"
-      case .attribute(_, isTransformable: let isTransformable) :
-        return isTransformable ? "TransformableAttribute" : "Attribute"
-    }
-  }
-}
 
+    var description: String {
+        switch self {
+        case .plain: return "plain"
+        case .relationship(_): return "Relationship"
+        case .attribute(_, let isTransformable):
+            return isTransformable ? "TransformableAttribute" : "Attribute"
+        }
+    }
+}
 
 // MARK: - Builder
 
 extension ModelMacro {
-  
-  // errorNode is the @Model, which is where we want to attach diagnostics
-  static func findModelProperties(in classDecl: ClassDeclSyntax,
-                                  errorNode: AttributeSyntax,
-                                  context: some MacroExpansionContext)
-              -> [ ModelProperty ]
-  {
-    var properties = [ ModelProperty ]()
-    
-    for member : MemberBlockItemSyntax in classDecl.memberBlock.members {
-      // Note: One "member block" can contain multiple variable declarations.
-      // Like in: `let a = 5, b = 6`.
-      addModelProperties(in: member.decl, to: &properties,
-                         context: context)
+
+    // errorNode is the @Model, which is where we want to attach diagnostics
+    static func findModelProperties(
+        in classDecl: ClassDeclSyntax,
+        errorNode: AttributeSyntax,
+        context: some MacroExpansionContext
+    )
+        -> [ModelProperty]
+    {
+        var properties = [ModelProperty]()
+
+        for member: MemberBlockItemSyntax in classDecl.memberBlock.members {
+            // Note: One "member block" can contain multiple variable declarations.
+            // Like in: `let a = 5, b = 6`.
+            addModelProperties(
+                in: member.decl, to: &properties,
+                context: context)
+        }
+
+        return properties
     }
 
-    return properties
-  }
-
-  /**
+    /**
    * This creates a `ModelProperty` object/value for each property in the
    * given `VariableDeclSyntax`.
    *
@@ -156,156 +156,162 @@ extension ModelMacro {
    *
    * It uses the `propertyType` function below to look for the attributes.
    */
-  static func addModelProperties<T>(in member: T,
-                                    to properties: inout [ ModelProperty ],
-                                    context: some MacroExpansionContext)
-    where T: SyntaxProtocol
-  {
-    // Note: One "member block" can contain multiple variable declarations.
-    // Like in: `let a = 5, b = 6`.
-    guard let variables = member.as(VariableDeclSyntax.self) else {
-      return
-    }
-    guard !variables.isStaticOrClass else { return }
-
-    // Those apply to all variables in the declaration!
-    let ( propertyType, isTransient ) = propertyType(
-      for: variables.attributes, context: context
+    static func addModelProperties<T>(
+        in member: T,
+        to properties: inout [ModelProperty],
+        context: some MacroExpansionContext
     )
-    
-    // Each binding is a variable in a declaration list,
-    // e.g. `let a = 5, b = 6`, the `a` and `b` would be bindings.
-    for binding : PatternBindingSyntax in variables.bindings {
-      guard binding.accessorBlock == nil else {
-        // Either this is a computed property or _Swift_ property observers,
-        // which are not allowed w/ `@NSManaged`.
-        continue
-      }
-          
-      guard let pattern = binding.pattern.as(IdentifierPatternSyntax.self) else
-      {
-        continue
-      }
-      let name = pattern.identifier.trimmed.text
-      
-      guard !forbiddenPropertyNames.contains(name) else {
-        context.diagnose(.propertyNameIsForbidden,
-                         on: member)
-        continue
-      }
+    where T: SyntaxProtocol {
+        // Note: One "member block" can contain multiple variable declarations.
+        // Like in: `let a = 5, b = 6`.
+        guard let variables = member.as(VariableDeclSyntax.self) else {
+            return
+        }
+        guard !variables.isStaticOrClass else { return }
 
-      properties.append(ModelProperty(
-        binding: binding,
-        type: propertyType,
-        name: name,
-        declaredValueType: binding.typeAnnotation?.type, // w/o the `:`
-        isTransient: isTransient,
-        initExpression: binding.initializer?.value // w/o the `=`
-      ))
-    }
-  }
-  
-  private static func propertyType(
-    for attributes: AttributeListSyntax,
-    context: some MacroExpansionContext
-  ) -> ( propertyType : ModelProperty.PropertyType, isTransient: Bool )
-  {
-    // Those apply to all variables in the declaration!
-    var propertyType = ModelProperty.PropertyType.plain
-    var isTransient  = false
-    
-    for attribute : AttributeListSyntax.Element in attributes {
-      switch attribute {
-        case .ifConfigDecl(_):
-          continue
-          
-        case .attribute(let attribute):
-          guard let name = attribute
-            .attributeName.as(IdentifierTypeSyntax.self)?
-            .name.trimmed.text else
-          {
-            continue
-          }
-          switch name {
-            case "Transient":
-              assert(!isTransient, "Transient macro applied twice!")
-              isTransient = true
-              
-            case "Attribute":
-              switch propertyType {
-                case .plain:
-                  propertyType = .attribute(
-                    attribute,
-                    isTransformable: isTransformableAttribute(attribute)
-                  )
-                case .attribute(_, _):
-                  context.diagnose(.multipleAttributeMacrosAppliedOnProperty,
-                                   on: attributes)
-                case .relationship(_):
-                  context.diagnose(
-                    .attributeAndRelationshipMacrosAppliedOnProperty,
-                    on: attributes
-                  )
-              }
-              
-            case "Relationship":
-              switch propertyType {
-                case .plain:
-                  propertyType = .relationship(attribute)
-                case .relationship(_):
-                  context.diagnose(
-                    .multipleRelationshipMacrosAppliedOnProperty,
-                    on: attributes
-                  )
-                case .attribute(_, _):
-                  context.diagnose(
-                    .attributeAndRelationshipMacrosAppliedOnProperty,
-                    on: attributes
-                  )
-              }
-              
-            default:
-              break
-          }
-          
-      }
+        // Those apply to all variables in the declaration!
+        let (propertyType, isTransient) = propertyType(
+            for: variables.attributes, context: context
+        )
+
+        // Each binding is a variable in a declaration list,
+        // e.g. `let a = 5, b = 6`, the `a` and `b` would be bindings.
+        for binding: PatternBindingSyntax in variables.bindings {
+            guard binding.accessorBlock == nil else {
+                // Either this is a computed property or _Swift_ property observers,
+                // which are not allowed w/ `@NSManaged`.
+                continue
+            }
+
+            guard let pattern = binding.pattern.as(IdentifierPatternSyntax.self) else {
+                continue
+            }
+            let name = pattern.identifier.trimmed.text
+
+            guard !forbiddenPropertyNames.contains(name) else {
+                context.diagnose(
+                    .propertyNameIsForbidden,
+                    on: member)
+                continue
+            }
+
+            properties.append(
+                ModelProperty(
+                    binding: binding,
+                    type: propertyType,
+                    name: name,
+                    declaredValueType: binding.typeAnnotation?.type,  // w/o the `:`
+                    isTransient: isTransient,
+                    initExpression: binding.initializer?.value  // w/o the `=`
+                ))
+        }
     }
 
-    return ( propertyType, isTransient )
-  }
- 
-  // Check whether the attribute specified a `.transformable` option.
-  // Check for: `@Attribute(.transformable(by: xx))`,
-  // Signature: (_ options: Option..., originalName...)
-  private static func isTransformableAttribute(_ syntax: AttributeSyntax)
-                      -> Bool
-  {
-    guard let arguments = syntax.arguments,
-          case .argumentList(let labeledExpressions) = arguments else
+    private static func propertyType(
+        for attributes: AttributeListSyntax,
+        context: some MacroExpansionContext
+    ) -> (propertyType: ModelProperty.PropertyType, isTransient: Bool) {
+        // Those apply to all variables in the declaration!
+        var propertyType = ModelProperty.PropertyType.plain
+        var isTransient = false
+
+        for attribute: AttributeListSyntax.Element in attributes {
+            switch attribute {
+            case .ifConfigDecl(_):
+                continue
+
+            case .attribute(let attribute):
+                guard
+                    let name = attribute
+                        .attributeName.as(IdentifierTypeSyntax.self)?
+                        .name.trimmed.text
+                else {
+                    continue
+                }
+                switch name {
+                case "Transient":
+                    assert(!isTransient, "Transient macro applied twice!")
+                    isTransient = true
+
+                case "Attribute":
+                    switch propertyType {
+                    case .plain:
+                        propertyType = .attribute(
+                            attribute,
+                            isTransformable: isTransformableAttribute(attribute)
+                        )
+                    case .attribute(_, _):
+                        context.diagnose(
+                            .multipleAttributeMacrosAppliedOnProperty,
+                            on: attributes)
+                    case .relationship(_):
+                        context.diagnose(
+                            .attributeAndRelationshipMacrosAppliedOnProperty,
+                            on: attributes
+                        )
+                    }
+
+                case "Relationship":
+                    switch propertyType {
+                    case .plain:
+                        propertyType = .relationship(attribute)
+                    case .relationship(_):
+                        context.diagnose(
+                            .multipleRelationshipMacrosAppliedOnProperty,
+                            on: attributes
+                        )
+                    case .attribute(_, _):
+                        context.diagnose(
+                            .attributeAndRelationshipMacrosAppliedOnProperty,
+                            on: attributes
+                        )
+                    }
+
+                default:
+                    break
+                }
+
+            @unknown default: continue
+            }
+        }
+
+        return (propertyType, isTransient)
+    }
+
+    // Check whether the attribute specified a `.transformable` option.
+    // Check for: `@Attribute(.transformable(by: xx))`,
+    // Signature: (_ options: Option..., originalName...)
+    private static func isTransformableAttribute(_ syntax: AttributeSyntax)
+        -> Bool
     {
-      return false
-    }
+        guard let arguments = syntax.arguments,
+            case .argumentList(let labeledExpressions) = arguments
+        else {
+            return false
+        }
 
-    for labeledExpression in labeledExpressions {
-      guard labeledExpression.label == nil else { break }
-      
-      guard let funCall = labeledExpression.expression
-                            .as(FunctionCallExprSyntax.self),
-            let member = funCall.calledExpression
-                            .as(MemberAccessExprSyntax.self)
-      else {
-        continue
-      }
-      guard case .identifier(let name) =
-              member.declName.baseName.tokenKind else
-      {
-        continue
-      }
-      
-      // Could be more advanced :-)
-      if name == "transformable" { return true }
+        for labeledExpression in labeledExpressions {
+            guard labeledExpression.label == nil else { break }
+
+            guard
+                let funCall = labeledExpression.expression
+                    .as(FunctionCallExprSyntax.self),
+                let member = funCall.calledExpression
+                    .as(MemberAccessExprSyntax.self)
+            else {
+                continue
+            }
+            guard
+                case .identifier(let name) =
+                    member.declName.baseName.tokenKind
+            else {
+                continue
+            }
+
+            // Could be more advanced :-)
+            if name == "transformable" { return true }
+        }
+
+        return false
     }
-    
-    return false
-  }
 }
